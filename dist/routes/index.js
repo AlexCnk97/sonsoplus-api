94180525779e7d100f6c68e4fe3ca4fe48a9da51
AlexCnk97 <alexchinke97@gmail.com>
2020-11-28 17:06:11 -0600

    slider done 4

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -8,8 +8,11 @@ exports.default = void 0;
 var _express = require("express");
 
 const router = (0, _express.Router)();
+const entretenimiento = ["https://img.europapress.es/fotoweb/fotonoticia_20200507140128_1024.jpg", "https://s3-sa-east-1.amazonaws.com/tvquality-epg/HISTORY/Forged+in+Fire/ForgedInFire.jpg?crop=resizeScaleAndCrop&size=THUMB", "https://www.nauticalnewstoday.com/wp-content/uploads/2018/03/Screeny-Shot-13-mar-2018-12.30.37-1.png", "https://img.europapress.es/fotoweb/fotonoticia_20200507140128_1024.jpg", "https://s3-sa-east-1.amazonaws.com/tvquality-epg/HISTORY/Forged+in+Fire/ForgedInFire.jpg?crop=resizeScaleAndCrop&size=THUMB", "https://www.nauticalnewstoday.com/wp-content/uploads/2018/03/Screeny-Shot-13-mar-2018-12.30.37-1.png"];
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    entretenimiento: entretenimiento
+  });
 });
 var _default = router;
 exports.default = _default;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,19 @@ import {Router} from 'express'
 
 const router = Router()
 
+const entretenimiento = [
+    "https://img.europapress.es/fotoweb/fotonoticia_20200507140128_1024.jpg",
+    "https://s3-sa-east-1.amazonaws.com/tvquality-epg/HISTORY/Forged+in+Fire/ForgedInFire.jpg?crop=resizeScaleAndCrop&size=THUMB",
+    "https://www.nauticalnewstoday.com/wp-content/uploads/2018/03/Screeny-Shot-13-mar-2018-12.30.37-1.png",
+    "https://img.europapress.es/fotoweb/fotonoticia_20200507140128_1024.jpg",
+    "https://s3-sa-east-1.amazonaws.com/tvquality-epg/HISTORY/Forged+in+Fire/ForgedInFire.jpg?crop=resizeScaleAndCrop&size=THUMB",
+    "https://www.nauticalnewstoday.com/wp-content/uploads/2018/03/Screeny-Shot-13-mar-2018-12.30.37-1.png",
+]
+
 router.get('/', (req,res)=>{
-    res.render('index')
+    res.render('index',{
+        entretenimiento: entretenimiento 
+    })
 })
 
 export default router;
